eec5598d13f5e966b8f9a1887eb524fbdcf3a6a5
Ian Filipe <ssianfilipe@gmail.com>
2020-06-02 19:25:59 -0300

    Update create-point.js

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,17 +1,5 @@
-/* 
-
-O que se repete?
-
-.id e .nome
-
-O que muda?
-
-url
-elemento select
-
-*/
-
 const stateSelect = document.querySelector("select[name=state]")
+const citySelect = document.querySelector("select[name=city]")
 
 function fetchTest(url, select) {
     fetch(url)
@@ -26,7 +14,6 @@ function fetchTest(url, select) {
 fetchTest(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`, stateSelect)
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
     const stateInput = document.querySelector("input[name=state]")
 
     const stateValue = event.target.value
@@ -41,49 +28,3 @@ function getCities(event) {
 document
     .querySelector("select[name=state]")
     .addEventListener("change", getCities)
-
-/*
-
-function populateStates() {
-    const stateSelect = document.querySelector("select[name=state]")
-
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() ) // retornando um valor de forma simples
-    .then( states => {
-
-        for ( state of states ) {
-            stateSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-
-        }
-
-    })
-}
-
-populateStates()
-
-function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("input[name=state]")
-
-    const stateValue = event.target.value
-
-    const indexOfSelectedValue = event.target.selectedIndex
-    stateInput.value = event.target.options[indexOfSelectedValue].text
-
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateValue}/municipios`
-    fetch(url)
-    .then( res => res.json() ) // retornando um valor de forma simples
-    .then( cities => {
-
-        for ( city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-        }
-        citySelect.disabled = false
-    })
-}
-
-document
-    .querySelector("select[name=state]")
-    .addEventListener("change", getCities)
-
-*/
