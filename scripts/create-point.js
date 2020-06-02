4e08a8b3488c67569b6df221eb52dde8ef080ac0
Ian Filipe <ssianfilipe@gmail.com>
2020-06-02 18:51:35 -0300

    Clean the code

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,49 @@
+/* 
+
+O que se repete?
+
+.id e .nome
+
+O que muda?
+
+url
+elemento select
+
+*/
+
+const stateSelect = document.querySelector("select[name=state]")
+
+function fetchTest(url, select) {
+    fetch(url)
+    .then( res => res.json() )
+    .then( data => {
+        for ( d of data ) {
+            select.innerHTML += `<option value="${d.id}">${d.nome}</option>`
+        }
+    })
+}
+
+fetchTest(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`, stateSelect)
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const stateValue = event.target.value
+
+    const indexOfSelectedValue = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedValue].text
+
+    fetchTest(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateValue}/municipios`, citySelect)
+    citySelect.disabled = false
+}
+
+document
+    .querySelector("select[name=state]")
+    .addEventListener("change", getCities)
+
+/*
+
 function populateStates() {
     const stateSelect = document.querySelector("select[name=state]")
 
@@ -39,3 +85,5 @@ function getCities(event) {
 document
     .querySelector("select[name=state]")
     .addEventListener("change", getCities)
+
+*/
